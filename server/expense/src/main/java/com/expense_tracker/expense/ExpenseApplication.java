99b8e53115e13a5999c9a1b56540c29db9a82f5e
Himanshu Singh <78968566+Himanshu1801@users.noreply.github.com>
2025-04-11 11:50:56 +0530

    Refactor POM and application properties for dotenv integration
    - Updated pom.xml to include java-dotenv dependency for environment variable management.
    - Modified application.properties to use ${MONGO_URI} placeholder for MongoDB URI instead of hardcoded values.
    - Enhanced ExpenseApplication to load environment variables from .env file.

--- server/expense/src/main/java/com/expense_tracker/expense/ExpenseApplication.java
+++ server/expense/src/main/java/com/expense_tracker/expense/ExpenseApplication.java
@@ -3,10 +3,15 @@ package com.expense_tracker.expense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ExpenseApplication {
 
 	public static void main(String[] args) {
+		// Load the .env file
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
 		SpringApplication.run(ExpenseApplication.class, args);
 	}
 }
